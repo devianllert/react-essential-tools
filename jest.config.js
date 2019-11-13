65f2516aeadb7f3eef9ecf399e4a7e66c192d88a
devianllert <devianllert@gmail.com>
2019-11-13 20:41:44 +0300

    fix: refactor tests env

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$', '^.+\\.module\\.(css|sass|scss)$'],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+};
